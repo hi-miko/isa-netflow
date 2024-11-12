016350cbcb851a8250410c3c186a856f87eb2a37
Patrik Uher <patrikuher35@gmail.com>
2024-11-12 18:01:32 +0100

    Created a struct that stores the needed packet information

---
+++ src/pack-info.hpp
@@ -0,0 +1,25 @@
+#ifndef PACK_INFO_HPP
+#define PACK_INFO_HPP
+
+#include <cstdint>
+
+struct packet_info
+{
+    uint32_t src_ip;
+    uint32_t dst_ip;
+
+    uint32_t ip_octets;
+    // timestamp relative to the export program
+    int32_t relative_timestamp;
+    
+    uint16_t src_port;
+    uint16_t dst_port;
+
+    uint8_t tcp_flags;
+    uint8_t ip_proto_type;
+
+};
+
+using pack_info = struct packet_info;
+
+#endif // FLOW_MANAGER_HPP
